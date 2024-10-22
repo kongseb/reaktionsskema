8f3986bbe3d4efaef2b3e7dfd5c22474e4e55a61
kongseb <sebastianemilhansen@yahoo.dk>
2024-10-22 21:23:21 +0200

    Nyt favicon

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
             <div className="relative">
               <FlaskConical className="text-green-600 w-12 h-12" />
               <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-bold text-xs">
-                
+              <link rel="icon" href="/favicon.ico" sizes="any" />
               </div>
             </div>
             <h1 className="text-3xl font-bold text-gray-800">Reaktionsskema.dk</h1>
